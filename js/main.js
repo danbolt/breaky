77fabb5e1944010a8d0f88b8982bd41cfbf3fd8a
Daniel Savage <dansvg@gmail.com>
2014-08-17 21:21:32 -0700

    Added a simple state machine.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
+var currentRecipe = null;
+var currentState = null;
+
+var updateView = function(text)
+{
+  $('#slideContent').html('<h1>' + text + '</h1>');
+}
+
+var applyRecipeData = function(recipeData)
+{
+  var stateArray = [];
+
+  recipeData.steps.forEach(function(step)
+  {
+    stateArray[step.name] = step;
+  });
+
+  recipeData.steps = stateArray;
+
+  currentRecipe = recipeData;
+  currentState = currentRecipe.firstStep;
+
+  updateView(currentRecipe.steps[currentState].content.mainText);
+}
+
+var nextState = function()
+{
+  if (currentState != null && currentRecipe != null)
+  {
+    currentState = currentRecipe.steps[currentState].nextStep;
+
+    if (currentState != null)
+    {
+      updateView(currentRecipe.steps[currentState].content.mainText);
+    }
+    else
+    {
+      updateView('You\'re done!');
+    }
+  }
+}
+
+var requestFailed = function(jqxhr, textStatus, error)
+{
+  var err = textStatus + ", " + error;
+  console.log( "Request Failed: " + err );
+}
+
+
 $('#slideContent').html('<h1>BREAKY</h1>');
+
+$.getJSON('recipes/crepes.json').done(applyRecipeData).fail(requestFailed);
+
+$('#slideContent').click(nextState);
